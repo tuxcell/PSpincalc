375ce0f3c5575e0a303998de18ad4d461fea3687
tuxcell <josephgama@yahoo.com>
2019-09-15 20:04:33 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -7,8 +7,10 @@ setuptools.setup(
     name="PSpincalc",
     version="0.2.3",
     author="Jose Gama",
-    author_email="rxprtgama@gmail.com",
+    author_email="josephgama@yahoo.com",
     description="Package for converting between attitude representations (DCM, Euler angles, Quaternions, and Euler vectors)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/tuxcell/PSpincalc",
     packages=setuptools.find_packages(),
     license="GNU General Public License v3 or later (GPLv3+)"

f9d28db3beedb3c29541b5d75b2117bcf2bb6246
tuxcell <josephgama@yahoo.com>
2019-09-18 23:22:28 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PSpincalc",
-    version="0.2.3",
+    version="0.2.4",
     author="Jose Gama",
     author_email="josephgama@yahoo.com",
     maintainer="tuxcell",

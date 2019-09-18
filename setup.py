a7a5ac94f321e188af5018e966275707a8219fda
tuxcell <josephgama@yahoo.com>
2019-09-18 23:05:25 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "License :: GNU General Public License v3 or later (GPLv3+)",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3',
